4d40d6998ad117318c666495a748c3c3b7e7b4b7
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-28 21:18:31 +0800

    pseudocode

---
+++ left_justify_string.py
@@ -0,0 +1,11 @@
+# Inducil, Raphael Clouiee A.
+# 3-28-25
+# batch 6, prog06: add space characters at the end of the string to complete the specified length without using ljust()
+
+# ask the user to enter a string
+# ask the user to enter the total width of the final string
+# get the length of the original string
+# calculate the number of spaces needed to reach the specified width
+# append the required number of spaces to the string
+# print the modified string
+# end
