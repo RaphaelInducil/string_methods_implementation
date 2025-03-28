ff815b7cbdb31716e14d8861ff28981ae34b258c
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-28 21:26:04 +0800

    pseudocode

---
+++ center_align_string.py
@@ -0,0 +1,11 @@
+# Inducil, Raphael Clouiee A.
+# 3-28-25
+# batch 6, prog07: add space characters at the beginning and at the end of the string to print it at the center without using center()
+
+# ask the user to enter a string
+# ask the user to enter the total width of the final string
+# get the length of the original string
+# calculate the number of spaces needed on each side to center the string
+# append spaces to both sides of the string
+# print the modified string
+# end
