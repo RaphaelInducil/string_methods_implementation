4460986adddf2a62b16e6ed6b8fbe12726e11208
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-28 20:51:37 +0800

    pseudocode

---
+++ remove_prefix_from_string.py
@@ -0,0 +1,10 @@
+# Inducil, Raphael Clouiee A.
+# 3-28-25
+# batch 6, prog02: remove the characters at the beginning of the string that match the given prefix without using removeprefix()
+
+# Ask the user to enter a string
+# Ask the user to enter a prefix to remove
+# Check if the string starts with the given prefix
+# If it does, remove the prefix by slicing the string
+# Print the modified string
+# End
