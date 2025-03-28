e49526213bfc0d12d493071aeaaaaa4369ae6a3a
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-28 21:33:31 +0800

    pseudocode

---
+++ capitalize_first_letter.py
@@ -0,0 +1,11 @@
+# Inducil, Raphael Clouiee A.
+# 3-28-25
+# batch 6, prog09: capitalize the first letter of the string and convert all other letters to lowercase without using capitalize()
+
+# ask the user to enter a string
+# check if the string is not empty
+# convert the first character to uppercase
+# convert the rest of the characters to lowercase
+# concatenate the modified characters to form the final string
+# print the modified string
+# end
