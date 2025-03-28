93b7713fd7aa0713bcadfc2b8f2991613500a465
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-28 20:41:43 +0800

    pseudocode

---
+++ remove_leading_spaces.py
@@ -0,0 +1,9 @@
+# Inducil, Raphael Clouiee A.
+# 3-28-25
+# batch 6, remove the space characters at the beginning of the string without using lstrip()
+
+# ask user to input a string with leading spaces
+# iterate through the string to find the first non-space character
+# create a new string starting from the first non-space character
+# print the new string without leading spaces
+# end
