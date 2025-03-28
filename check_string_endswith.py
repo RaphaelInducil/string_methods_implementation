03c51191f26061ee31b9613e35aeb482e47ddb72
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-28 21:11:39 +0800

    pseudocode

---
+++ check_string_endswith.py
@@ -0,0 +1,12 @@
+# Inducil, Raphael Clouiee A.
+# 3-28-25
+# batch 6, prog05: check if the string end part matches the function parameter without using endswith()
+
+# ask the user to enter a string
+# ask the user to enter the ending substring to check 
+# get the length of the ending substring
+# compare the last characters of the string with the ending substring
+# if they match, return true
+# otherwise, return false
+# print the result
+# end
