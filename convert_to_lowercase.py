d3a970ec81f00f8161b3f05437e15668a469c1f5
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-28 20:56:49 +0800

    pseudocode

---
+++ convert_to_lowercase.py
@@ -0,0 +1,11 @@
+# Inducil, Raphael Clouiee A.
+# 3-28-25
+# batch 6, prog03: convert all characters of the string into lowercase without using lower()
+
+# ask the user to enter a string
+# initialize an empty string to store the lowercase result
+# iterate through each character in the string
+# if the character is an uppercase letter, convert it to lowercase manually
+# append the character to the new string
+# print the modified string
+# end
