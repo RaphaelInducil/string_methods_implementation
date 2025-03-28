1be601f452bc06afc09c3d2812b4a7d80bee7433
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-28 21:29:29 +0800

    pseudocode

---
+++ swap_case_of_string.py
@@ -0,0 +1,12 @@
+# Inducil, Raphael Clouiee A.
+# 3-28-25
+# batch 6, prog08: reverse the casing of each character in the string without using swapcase()
+
+# ask the user to enter a string
+# initialize an empty string to store the swapped case result
+# iterate through each character in the input string
+# if the character is uppercase, convert it to lowercase
+# if the character is lowercase, convert it to uppercase
+# concatenate the modified characters to form the final string
+# print the modified string
+# end
