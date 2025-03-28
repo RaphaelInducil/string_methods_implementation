5071a92281988cab67fb0067d6de66b393fbea5c
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-28 21:38:42 +0800

    pseudocode

---
+++ title_case_string.py
@@ -0,0 +1,11 @@
+# Inducil, Raphael Clouiee A.
+# 3-28-25
+# batch 6, prog10: capitalize the first letter of each word in a string and convert all other letters to lowercase without using title()
+
+# ask the user to enter a string
+# split the string into words
+# capitalize the first letter of each word
+# convert the rest of the letters in each word to lowercase
+# join the modified words back into a single string
+# print the modified string
+# end
