fe24961cfe89a555f8f4df985cce0c7c61627509
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-01 22:38:03 +0800

    pseudocode

---
+++ advanced_string_manipulation/check_if_lowercase.py
@@ -0,0 +1,12 @@
+# Inducil, Raphael Clouiee A.
+# 4-01-25 
+# batch 7, Prog04. islower() checks if all characters of the string are in lowercase.
+# Create a program that does the same functionality without using islower() function.
+
+# ask the user to enter a string
+# initialize a variable to track if all characters are lowercase
+# loop through each character in the string
+# check if the character is an uppercase letter
+# if an uppercase letter is found, set the variable to False and break the loop
+# print whether the string is entirely in lowercase
+# end
