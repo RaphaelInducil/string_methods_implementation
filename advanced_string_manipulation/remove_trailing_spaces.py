b524cd04a58903f08699e9c9db181a7c4916afe5
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-01 22:18:44 +0800

    pseudocode

---
+++ advanced_string_manipulation/remove_trailing_spaces.py
@@ -0,0 +1,11 @@
+# Inducil, Raphael Clouiee A.
+# 4-01-25 
+# batch 7, Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+# ask the user to enter a string
+# loop through the string from the end
+# remove space characters from the end until a non-space character is found
+# return the modified string
+# print the modified string
+# end
+
