a9852f57b1f74dfd02a3d2fc1beab7a65d732e3b
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-01 22:51:23 +0800

    pseudocode

---
+++ advanced_string_manipulation/count_substring.py
@@ -0,0 +1,14 @@
+# Inducil, Raphael Clouiee A.
+# 4-01-25 
+# batch 7, Prog08. count() returns how many times the function parameter 
+# appears in the string. Create a program that does the same functionality 
+# without using count() function.
+
+# ask the user to enter a string
+# ask the user to enter the substring to count
+# initialize a counter variable to zero
+# get the length of the substring
+# loop through the string while checking each position for a match
+# if a match is found, increment the counter
+# print the total count of occurrences
+# end
