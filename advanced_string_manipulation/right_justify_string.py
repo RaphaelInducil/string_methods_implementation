774678e7d57ab0b278005c4885b56279956af573
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-01 22:42:16 +0800

    pseudocode

---
+++ advanced_string_manipulation/right_justify_string.py
@@ -0,0 +1,12 @@
+# Inducil, Raphael Clouiee A.
+# 4-01-25 
+# batch 7, Prog06. rjust() adds space characters at the beginning of the string 
+# to complete the number of characters specified in the function parameter.
+# Create a program that does the same functionality without using rjust() function.
+
+# ask the user to enter a string
+# ask the user to enter the total width of the output string
+# calculate the number of spaces needed at the beginning
+# create a new string with the required spaces followed by the original string
+# print the right-justified string
+# end
