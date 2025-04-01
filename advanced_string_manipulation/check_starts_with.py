2da902f3b58ba96894ac9cda7f491f3534a8e9e1
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-01 22:40:36 +0800

    pseudocode

---
+++ advanced_string_manipulation/check_starts_with.py
@@ -0,0 +1,13 @@
+# Inducil, Raphael Clouiee A.
+# 4-01-25 
+# batch 7, Prog05. startswith() checks if the beginning part of the string matches 
+# the function parameter. Create a program that does the same functionality 
+# without using startswith() function.
+
+# ask the user to enter a string
+# ask the user to enter a prefix to check
+# get the length of the prefix
+# extract the beginning part of the string with the same length as the prefix
+# compare the extracted part with the prefix
+# print whether the string starts with the given prefix
+# end
