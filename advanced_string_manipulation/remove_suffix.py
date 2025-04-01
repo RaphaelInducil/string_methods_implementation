b29d072f663cc90a3ba6978d782f9aff2a8130be
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-01 22:32:43 +0800

    pseudocode

---
+++ advanced_string_manipulation/remove_suffix.py
@@ -0,0 +1,12 @@
+# Inducil, Raphael Clouiee A.
+# 4-01-25 
+# batch 7, Prog02. removesuffix() remove the characters at the end of the string 
+# that matches the function parameter. Create a program that do the same functionality 
+# without using removesuffix() function.
+
+# ask the user to enter a string
+# ask the user to enter a suffix to remove
+# check if the string ends with the given suffix
+# if the string ends with the suffix, slice the string to remove the suffix
+# print the modified string
+# end
