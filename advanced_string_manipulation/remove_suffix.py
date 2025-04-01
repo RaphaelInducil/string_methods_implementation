384fabd4e8b6d548221cabcb68b751b922d59ed5
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-01 22:33:47 +0800

    Ask the user for input, check if the string ends with the given suffix, remove the suffix if present, and display the modified string.

--- advanced_string_manipulation/remove_suffix.py
+++ advanced_string_manipulation/remove_suffix.py
@@ -5,8 +5,25 @@
 # without using removesuffix() function.
 
 # ask the user to enter a string
+
+input_string = input("Enter a string: ")
+
 # ask the user to enter a suffix to remove
+
+suffix = input("Enter the suffix to remove: ")
+
 # check if the string ends with the given suffix
-# if the string ends with the suffix, slice the string to remove the suffix
+
+if input_string.endswith(suffix):
+
+    # if the string ends with the suffix, slice the string to remove the suffix
+
+    modified_string = input_string[:-len(suffix)]
+else:
+    modified_string = input_string
+
 # print the modified string
+
+print("Modified string:", modified_string)
+
 # end
