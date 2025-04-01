79f5b88bd21bd1d1e919ff3f3c3e90c90fdce618
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-01 23:02:51 +0800

    pseudocode

---
+++ advanced_string_manipulation/find_last_substring_index.py
@@ -0,0 +1,14 @@
+# Inducil, Raphael Clouiee A.
+# 4-01-25 
+# batch 7, Prog10. rindex() returns the first location of the function parameter 
+# in the string starting from the last character. Create a program that does 
+# the same functionality without using rindex() function.
+
+# ask the user to enter a string
+# ask the user to enter the substring to find
+# get the length of the substring
+# initialize a variable to store the index, set to -1 (not found)
+# loop through the string in reverse order while checking each position for a match
+# if a match is found, store the index and break the loop
+# check if the substring was found and print the result
+# end
