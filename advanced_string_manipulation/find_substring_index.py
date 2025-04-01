08d53ba590809b97f849b1494096b2de3c36423d
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-01 22:58:44 +0800

    pseudocode

---
+++ advanced_string_manipulation/find_substring_index.py
@@ -0,0 +1,13 @@
+# Inducil, Raphael Clouiee A.
+# 4-01-25 
+# batch 7, Prog09. index() returns the first location of the function parameter 
+# in the string. Create a program that does the same functionality 
+# without using index() function.
+
+# ask the user to enter a string
+# ask the user to enter the substring to find
+# get the length of the substring
+# loop through the string while checking each position for a match
+# if a match is found, print the index and exit the loop
+# if no match is found, print a message indicating that the substring is not present
+# end
